47d9abf0e98e7dc88de37078dd1f73d771ed496a
felicity12345 <f.osawaru@rgu.ac.uk>
2022-03-25 12:40:31 -0700

    change

--- index.php
+++ index.php
@@ -1,31 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>welcome to Aberdeen</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css">
-   
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-   <a class="navbar-brand" href="index.php">ABZ</a>
-   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    
-  
-                
-                <li><a href="#">About Us</a></li>
-                <li><a href="register.php">Sign up</a></li>
-                <li><a href="login.php">Log in</a></li>
-                <li><a href="#">Admin</a></li>
-       </div>
-    </div>
-  </nav>
-</head>
-
+<?php 
+  include_once 'header.php';
+  ?>
    
     <header id="showcase">
         <h1>Welcome to Aberdeen</h1>
@@ -39,7 +14,7 @@
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro, fugiat deleniti. Rerum, dolorem! A, asperiores. Laudantium itaque nemo adipisci repudiandae consectetur culpa explicabo vero accusamus minima perspiciatis deserunt, quisquam quam quia minus aperiam unde? Autem expedita amet cumque quaerat delectus vitae asperiores, perspiciatis debitis corporis reprehenderit. Adipisci vero ex, porro obcaecati odit excepturi sit eos, iste ipsa voluptas ducimus minus exercitationem sint quisquam autem facere explicabo consequatur tempora tenetur voluptates. Possimus quod sequi, delectus dolorum consequatur magnam. Consequatur quae repudiandae corporis fuga cupiditate recusandae unde blanditiis officia, expedita harum accusantium iure totam et vel ab molestias quos dolor, ut a? ipsum dolor sit, amet consectetur adipisicing elit. Rerum, labore provident. Dicta sapiente numquam perspiciatis!</p>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima ea est, veritatis minus fugit obcaecati eum nihil exercitationem neque ratione ab eligendi similique libero voluptas, natus architecto hic at nemo possimus! Tenetur facere illo molestias distinctio pariatur aperiam natus minus odit veritatis sed exercitationem in sapiente voluptate repudiandae, asperiores delectus minima nisi excepturi nostrum iste adipisci porro. Repellat totam doloremque reiciendis dolorem quidem facere quas a, quisquam consequatur, sint aliquam voluptatum similique minus delectus cumque deserunt id. Adipisci quaerat consectetur eaque. Ad perspiciatis, voluptatem assumenda dolores optio nulla accusamus, natus quas quos quis recusandae corrupti numquam voluptate eaque iusto unde? ipsum dolor sit, amet consectetur adipisicing elit. Rerum, labore provident. Dicta sapiente numquam perspiciatis!</p>
        
-        <center> <p><a href="apearlregister.php" target="blank">click here</a> to find more about Aberdeen</p></center> 
+        <center> <p><a href="register.php" target="blank">click here</a> to find more about Aberdeen</p></center> 
       </div>
       <main>
         <aside>
