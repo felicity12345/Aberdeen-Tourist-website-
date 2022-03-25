32675d3c4a8cc0473a542c88bbc8b678e9a2d11f
felicity12345 <f.osawaru@rgu.ac.uk>
2022-03-25 12:42:25 -0700

    yes

--- header.php
+++ header.php
@@ -20,7 +20,7 @@
                 <li><a href="#">About Us</a></li>
                 <li><a href="register.php">Sign up</a></li>
                 <li><a href="login.php">Log in</a></li>
-                <li><a href="#">Admin</a></li>
+                
        </div>
     </div>
   </nav>
