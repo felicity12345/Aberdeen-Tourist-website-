1655f3437afd767e054b0657abbe94db29c4beb4
felicity12345 <f.osawaru@rgu.ac.uk>
2022-03-25 12:17:27 -0700

    yes

--- index.php
+++ index.php
@@ -1,6 +1,31 @@
-<?php 
-  include_once 'header.php';
-  ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>welcome to Aberdeen</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+   
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+   <a class="navbar-brand" href="index.php">ABZ</a>
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    
+  
+                
+                <li><a href="#">About Us</a></li>
+                <li><a href="register.php">Sign up</a></li>
+                <li><a href="login.php">Log in</a></li>
+                <li><a href="#">Admin</a></li>
+       </div>
+    </div>
+  </nav>
+</head>
+
    
     <header id="showcase">
         <h1>Welcome to Aberdeen</h1>
