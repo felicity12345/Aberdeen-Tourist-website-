563adef568cebeb85a1e91ec69e521530573f0d3
felicity12345 <f.osawaru@rgu.ac.uk>
2022-03-27 09:02:24 -0700

    YES

--- contact.php
+++ contact.php
@@ -1,38 +1,20 @@
 <?php
-// Initialize the session
-session_start();
-
-// Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: contact.php");
-    exit;
-}
-if(isset($_GET['logout'])){
-    
-  session_destroy();
-  unset($_SESSION['username']);
-  header("location : login.php");
-}
-?>
-
-
-<?php
  include("configure.php");
 
   if($_SERVER['REQUEST_METHOD'] == "POST")
 
   {
-    $fullname = $_POST['name'];
-    $email = $_POST['email'];
-    $subject= $_POST['contact'];
-    $message = $_POST['location'];
+    $name= $_POST['name'];
+    $email= $_POST['email'];
+    $subject= $_POST['subject'];
+    $message = $_POST['message'];
     
 
     //connection
 
-    if(!empty($name) && !empty($email) && !empty($contact) && !empty($location))
+    if(!empty($name) && !empty($email) && !empty($subject) && !empty($message))
     {
-      $query = "insert into contact (name, email, contact, location) values ('$name', '$email', '$contact', '$location')";
+      $query = "insert into contact (name, email, subject, message) values ('$name', '$email', '$subject', '$message')";
 
       mysqli_query($link, $query);
 
@@ -84,9 +66,9 @@ body{ font: 14px sans-serif;
 </style>
  <section class="my-4">
           <div class="w-50 m-auto">   
-    <form class="contact-form" action="contactform.php" method="post">
+    <form class="contact-form" action="contact.php" method="post">
            <input type="text" name="name" placeholder="Full name">
-           <input type="text" name="mail" placeholder="Your e-mail">
+           <input type="text" name="email" placeholder="Email">
            <input type="text" name="subject" placeholder="Subject">
            <textarea name="message" placeholder="message"></textarea>
         <center><button type="submit" class="btn btn- success"><h3>submit</h3></button></center>
