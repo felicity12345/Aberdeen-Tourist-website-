342818dde1a0624e35add6698341ed473f319129
felicity12345 <f.osawaru@rgu.ac.uk>
2022-03-27 17:26:11 -0700

    db

--- db.php
+++ db.php
@@ -1,25 +1,26 @@
 <?php
-/* Attempt MySQL server connection. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-$link = mysqli_connect("localhost", "root", "", "aberdeen");
+    //* connecting to a localhost with the db name and password *//
+        $link = mysqli_connect("localhost", "root", "", "aberdeen");
 
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
+    
+    // Check connection
+       if($link === false){
+        die("ERROR: Could not connect. " . mysqli_connect_error());
+        }
 
-// Attempt create table query execution
-$sql = "CREATE TABLE users (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(250) NOT NULL UNIQUE,
-    password VARCHAR(250) NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP)";
-if(mysqli_query($link, $sql)){
-    echo "  Tourist Table created successfully.";
-} else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-}
-
-// Close connection
-mysqli_close($link);
+  
+    //  creating table query execution
+        $sql = "CREATE TABLE users (
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        username VARCHAR(250) NOT NULL UNIQUE,
+        password VARCHAR(250) NOT NULL,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP)";
+        if(mysqli_query($link, $sql)){
+        echo "  Tourist Table created successfully.";
+     }    else{
+       echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+      }
+       
+    // Close connection
+        mysqli_close($link);
 ?>
