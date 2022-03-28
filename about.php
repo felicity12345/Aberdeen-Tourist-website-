6f58901c3e9e0ed503676c15296a674293b0c28b
felicity12345 <f.osawaru@rgu.ac.uk>
2022-03-27 18:01:58 -0700

    about

--- about.php
+++ about.php
@@ -1,49 +1,45 @@
-
-
 <?php 
   include_once 'header.php';
-  ?>
+?>
 <style>
-div.gallery {
-margin: 5px;
-background-position: center;
-height:100hv;
-display: flex;
-flex-direction: column;
-background-size: cover;
-}
+  div.gallery {
+  margin: 5px;
+  background-position: center;
+  height:100hv;
+  display: flex;
+  flex-direction: column;
+  background-size: cover;
+ }
 
 
-div.gallery img {
+  div.gallery img {
   width: 100%;
   height: auto;
-}
+ }
 
-div.desc {
+  div.desc {
   padding: 15px;
   
-}
 </style>
-</head>
+
 <body>
-
-<div class="gallery">
-  <a target="_blank" href="">
-    <img src="image/aberdeen3.jpeg" alt="Nature" width="600" height="400">
-  </a>
-  <div class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi suscipit a perferendis 
+ <div class="gallery">
+    <a target="_blank" href="">
+      <img src="image/aberdeen3.jpeg" alt="Nature" width="600" height="400">
+    </a>
+    <div class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi suscipit a perferendis 
       fuga aliquid? Sequi ullam voluptatibus dolorem omnis perferendis.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro, fugiat deleniti. Rerum, dolorem! A, asperiores. Laudantium itaque nemo adipisci repudiandae consectetur culpa explicabo vero accusamus minima perspiciatis deserunt, quisquam quam quia minus aperiam unde? Autem expedita amet cumque quaerat delectus vitae asperiores, perspiciatis debitis corporis reprehenderit. Adipisci vero ex, porro obcaecati odit excepturi sit eos, iste ipsa voluptas ducimus minus exercitationem sint quisquam autem facere explicabo consequatur tempora tenetur voluptates. Possimus quod sequi, delectus dolorum consequatur magnam. Consequatur quae repudiandae corporis fuga cupiditate recusandae unde blanditiis officia, expedita harum accusantium iure totam et vel ab molestias quos dolor, ut a? ipsum dolor sit, amet consectetur adipisicing elit. Rerum, labore provident. Dicta sapiente numquam perspiciatis!
-  </div>
-  <div class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi suscipit a perferendis 
+    </div>
+    <div class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi suscipit a perferendis 
       fuga aliquid? Sequi ullam voluptatibus dolorem omnis perferendis. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro, fugiat deleniti. Rerum, dolorem! A, asperiores. Laudantium itaque nemo adipisci repudiandae consectetur culpa explicabo vero accusamus minima perspiciatis deserunt, quisquam quam quia minus aperiam unde? Autem expedita amet cumque quaerat delectus vitae asperiores, perspiciatis debitis corporis reprehenderit. Adipisci vero ex, porro obcaecati odit excepturi sit eos, iste ipsa voluptas ducimus minus exercitationem sint quisquam autem facere explicabo consequatur tempora tenetur voluptates. Possimus quod sequi, delectus dolorum consequatur magnam. Consequatur quae repudiandae corporis fuga cupiditate recusandae unde blanditiis officia, expedita harum accusantium iure totam et vel ab molestias quos dolor, ut a? ipsum dolor sit, amet consectetur adipisicing elit. Rerum, labore provident. Dicta sapiente numquam perspiciatis!
-</div>
-</div>
-
-<center> <p><a href="register.php" target="blank">click here</a> to find more about Aberdeen</p></center> 
-      </div>
+    </div>
+ </div>
+ <center> <p><a href="register.php" target="blank">click here</a> to find more about Aberdeen</p></center> 
+ <p style="text-align:center; font size:0.85em">Copyright &copy; 2020 Aberdeen Tourism</p>
+      
         
-      <p style="text-align:center; font size:0.85em">Copyright &copy; 2020 Aberdeen Tourism</p>
+</body>
 
-    <?php 
+<?php 
   include_once 'footer.php';
-  ?>
+?>
