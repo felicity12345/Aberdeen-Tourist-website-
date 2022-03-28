71418dc25b097f519f717717badc8213ad9a4714
felicity12345 <f.osawaru@rgu.ac.uk>
2022-03-28 04:53:02 -0700

    header

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,35 +9,23 @@
     <link rel="stylesheet" href="css/style.css">
    
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-   <a class="navbar-brand" href="index.php">ABZ</a>
-   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    
-  
-                
-                <li><a href="about.php">About Us</a></li>
-                <li><a href="register.php">Sign up</a></li>
-                <li><a href="login.php">Log in</a></li>
-                
-              
-                
-       </div>
-    </div>
-  </nav>
-</head>
-
-
-
-
-
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+      <a class="navbar-brand" href="index.php">ABZ</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <li><a href="about.php">About Us</a></li>
+        <li><a href="register.php">Sign up</a></li>
+        <li><a href="login.php">Log in</a></li>
+        </div>
+      </div>
+    </nav>
+  </head>
+  <body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<body>
-    
-</body>
+  </body>
+</html>
 
     
