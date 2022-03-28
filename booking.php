f9ec6248465eb1b857dd37fc06e95df5edb0e7fd
felicity12345 <f.osawaru@rgu.ac.uk>
2022-03-28 06:24:27 -0700

    Booking details

--- booking.php
+++ booking.php
@@ -2,7 +2,7 @@
   // start session
    session_start();
 
-  // Check if the user is logged in, if not then redirect users to login page
+  // Check if the user is logged in, if not then redirect user to login page
  if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
