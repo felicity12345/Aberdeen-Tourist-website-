67418fdc68a714d7bf46cc41347b3d9e1a583449
felicity12345 <f.osawaru@rgu.ac.uk>
2022-03-28 04:59:20 -0700

    index

--- index.php
+++ index.php
@@ -1,32 +1,23 @@
 <?php 
   include_once 'header.php';
-  ?>
-   
-    <header id="showcase">
-        <h1>Welcome to Aberdeen</h1>
-        <p>A PLACE TO VISIT<br>FOR YOUR NEXT TRIP</p>
-        <p>The city is historically know for is strong<br>
-        ties with the nortth sea it is the oil<br> hub for europe. <br></p>
-        </header>
-    <body>
-     
-         <div style="padding: 0em 1em 1em">
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro, fugiat deleniti. Rerum, dolorem! A, asperiores. Laudantium itaque nemo adipisci repudiandae consectetur culpa explicabo vero accusamus minima perspiciatis deserunt, quisquam quam quia minus aperiam unde? Autem expedita amet cumque quaerat delectus vitae asperiores, perspiciatis debitis corporis reprehenderit. Adipisci vero ex, porro obcaecati odit excepturi sit eos, iste ipsa voluptas ducimus minus exercitationem sint quisquam autem facere explicabo consequatur tempora tenetur voluptates. Possimus quod sequi, delectus dolorum consequatur magnam. Consequatur quae repudiandae corporis fuga cupiditate recusandae unde blanditiis officia, expedita harum accusantium iure totam et vel ab molestias quos dolor, ut a? ipsum dolor sit, amet consectetur adipisicing elit. Rerum, labore provident. Dicta sapiente numquam perspiciatis!</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima ea est, veritatis minus fugit obcaecati eum nihil exercitationem neque ratione ab eligendi similique libero voluptas, natus architecto hic at nemo possimus! Tenetur facere illo molestias distinctio pariatur aperiam natus minus odit veritatis sed exercitationem in sapiente voluptate repudiandae, asperiores delectus minima nisi excepturi nostrum iste adipisci porro. Repellat totam doloremque reiciendis dolorem quidem facere quas a, quisquam consequatur, sint aliquam voluptatum similique minus delectus cumque deserunt id. Adipisci quaerat consectetur eaque. Ad perspiciatis, voluptatem assumenda dolores optio nulla accusamus, natus quas quos quis recusandae corrupti numquam voluptate eaque iusto unde? ipsum dolor sit, amet consectetur adipisicing elit. Rerum, labore provident. Dicta sapiente numquam perspiciatis!</p>
-       
-        <center> <p><a href="register.php" target="blank">click here</a> to find more about Aberdeen</p></center> 
-      </div>
-      <main>
-        <aside>
-          
-        </aside>
-      </main>
-        
-      <p style="text-align:center; font size:0.85em">Copyright &copy; 2020 Aberdeen Tourism</p>
-</body>
-
+?>
+<html>
+  <header id="showcase">
+    <h1>Welcome to Aberdeen</h1>
+    <p>A PLACE TO VISIT<br>FOR YOUR NEXT TRIP</p>
+    <p>The city is historically know for is strong<br>
+     ties with the nortth sea it is the oil<br> hub for europe. <br></p>
+  </header>
+  <body>
+    <div style="padding: 0em 1em 1em">
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro, fugiat deleniti. Rerum, dolorem! A, asperiores. Laudantium itaque nemo adipisci repudiandae consectetur culpa explicabo vero accusamus minima perspiciatis deserunt, quisquam quam quia minus aperiam unde? Autem expedita amet cumque quaerat delectus vitae asperiores, perspiciatis debitis corporis reprehenderit. Adipisci vero ex, porro obcaecati odit excepturi sit eos, iste ipsa voluptas ducimus minus exercitationem sint quisquam autem facere explicabo consequatur tempora tenetur voluptates. Possimus quod sequi, delectus dolorum consequatur magnam. Consequatur quae repudiandae corporis fuga cupiditate recusandae unde blanditiis officia, expedita harum accusantium iure totam et vel ab molestias quos dolor, ut a? ipsum dolor sit, amet consectetur adipisicing elit. Rerum, labore provident. Dicta sapiente numquam perspiciatis!</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima ea est, veritatis minus fugit obcaecati eum nihil exercitationem neque ratione ab eligendi similique libero voluptas, natus architecto hic at nemo possimus! Tenetur facere illo molestias distinctio pariatur aperiam natus minus odit veritatis sed exercitationem in sapiente voluptate repudiandae, asperiores delectus minima nisi excepturi nostrum iste adipisci porro. Repellat totam doloremque reiciendis dolorem quidem facere quas a, quisquam consequatur, sint aliquam voluptatum similique minus delectus cumque deserunt id. Adipisci quaerat consectetur eaque. Ad perspiciatis, voluptatem assumenda dolores optio nulla accusamus, natus quas quos quis recusandae corrupti numquam voluptate eaque iusto unde? ipsum dolor sit, amet consectetur adipisicing elit. Rerum, labore provident. Dicta sapiente numquam perspiciatis!</p>
+       <center> <p><a href="register.php" target="blank">click here</a> to find more about Aberdeen</p></center> 
+    </div>
+    <p style="text-align:center; font size:0.85em">Copyright &copy; 2020 Aberdeen Tourism</p>
+  </body>
+  </header>
 </html>
-</header>
-    <?php 
+<?php 
   include_once 'footer.php';
-  ?>
+?>
