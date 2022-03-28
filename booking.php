5f7876b68d17675b9fbb72a6377eea1ab19b1b34
felicity12345 <f.osawaru@rgu.ac.uk>
2022-03-27 17:09:37 -0700

    yes

--- booking.php
+++ booking.php
@@ -1,11 +1,3 @@
-
-<?php
-include("configure.php");
-$sql="SELECT * FROM book;";
-$result = mysqli_query($link, $sql);
-$resultCheck = mysqli_num_rows($result);
-?>
-
 <?php
 // Initialize the session
 session_start();
@@ -25,29 +17,24 @@ if(isset($_GET['logout'])){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    
     <title>fetch Data from database</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
-    
-
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="index.php">ABZ</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="index.php">ABZ</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav">
-      
-     <li class="nav-item dropdown">
+       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Categories
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-         
           <a class="dropdown-item" href="contact.php">Contact Us</a>
           <a class="dropdown-item" href="logout.php">Logout</a>
           
@@ -56,12 +43,23 @@ if(isset($_GET['logout'])){
     </ul>
   </div>
 </nav>
-
-  
 </head>
+
+
+
+<?php
+//connecting to database
+include("configure.php");
+
+//make sql
+$sql="SELECT * FROM book;";
+//get result
+$result = mysqli_query($link, $sql) or die(mysqli);
+$resultCheck = mysqli_num_rows($result);
+?>
     <body>
         <table align="center" boarder="3px" style="width:100%; line-height:40px;">
-       <center><h2>Your Booking Details</h2></center> 
+             <center><h2>Your Booking Details</h2></center> 
            <tr>
                 <th colspan="7"></th>
             </tr>
@@ -74,8 +72,9 @@ if(isset($_GET['logout'])){
                 <th> arrivals </th>
                 <th> leaving </th>
            </t>
-
+           
            <?php
+           //fetch result
            while ($rows =mysqli_fetch_assoc($result)){
            ?>
                <tr>
